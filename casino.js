ac8f2a25834864db3f5fe2646821c6657677f3ad
gharr27 <garrett.harr27@gmail.com>
2026-06-03 16:07:37 -0500

    Quality of Life and Blackjack Dialog
    Add "$" to the balance number
    Add functionality to open dialog box for Blackjack

--- casino.js
+++ casino.js
@@ -3,11 +3,24 @@ let balance = localStorage.getItem('balance') || 100;
 const balanceText = document.querySelector(".balance");
 
 const spinBtn = document.getElementById("spin");
+const blackjackDialog = document.getElementById("blackjackDialog");
+const blackjackOpenBtn = document.getElementById("blackjackOpen");
+const blackjackCloseBtn = document.getElementById("blackjackClose");
 
 updateBalance();
 
+function openBlackjack() {
+	console.log("open blackjack");
+	blackjackDialog.showModal();
+}
+
+function closeBlackjack() {
+	console.log("close blackjack");
+	blackjackDialog.close();
+}
+
 function updateBalance() {
-  balanceText.textContent = `Balance: ${balance}`;
+  balanceText.textContent = `Balance: $${balance}`;
   localStorage.setItem('balance', balance);
 }
 
@@ -18,15 +31,43 @@ function slotMachine() {
   console.log(spin);
   
   if (spin == 10) {
-    result.textContent = "You win!"
+    result.textContent = "You win!";
     balance += 10;
   }
   else {
-    result.textContent = "You Lose!"
+    result.textContent = "You Lose!";
     balance -= 1;
   }
   
   updateBalance();
 }
 
+function blackjack() {
+	//Bet $10
+	//Double if win
+	//Hit or Stay
+	//Dealer and player
+	
+	balance -= 10;
+	updateBalance();
+	
+	let playerHand = 0;
+	let dealerHand = 0;
+	
+	let choice = true;
+	
+	while (choice || playerHand >= 21) {
+		dealerHand += dealCard();
+		playerHand += dealCard();
+	}
+	
+	
+}
+
+funct dealCard() {
+	return Math.floor(Math.random() * 10) + 1;
+}
+
 spinBtn.addEventListener("click", slotMachine);
+blackjackOpenBtn.addEventListener("click", openBlackjack);
+blackjackCloseBtn.addEventListener("click", closeBlackjack);
