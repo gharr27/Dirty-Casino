3f2ba7843e44f0689685bef27788c87dded3fce0
gharr27 <garrett.harr27@gmail.com>
2026-06-04 13:56:34 -0500

    Add Blackjack backend
    Added basic Blackjack functionality, playable only through the console.

--- casino.js
+++ casino.js
@@ -1,24 +1,77 @@
 let balance = localStorage.getItem('balance') || 100;
+let playerHand = 0;
+let dealerHand = 0;
 
+//Elements
 const balanceText = document.querySelector(".balance");
-
-const spinBtn = document.getElementById("spin");
 const blackjackDialog = document.getElementById("blackjackDialog");
+
+//Buttons:
+const spinBtn = document.getElementById("spin");
 const blackjackOpenBtn = document.getElementById("blackjackOpen");
-const blackjackCloseBtn = document.getElementById("blackjackClose");
+const hitBtn = document.getElementById("hitBtn");
+const stayBtn = document.getElementById("stayBtn");
 
 updateBalance();
 
+//Blackjack
 function openBlackjack() {
 	console.log("open blackjack");
 	blackjackDialog.showModal();
+	
+	dealerHand = 0;
+	playerHand = 0;
+	
+	balance -= 10;
+	updateBalance();
+	
+	blackjack();
 }
-
 function closeBlackjack() {
-	console.log("close blackjack");
 	blackjackDialog.close();
 }
+function hit() {
+	blackjack();
+}
+function stay() {
+	
+	if (playerHand == dealerHand) {
+		console.log("Tie");
+		balance += 10;
+	}
+	else if (playerHand == 21) {
+		console.log("You win!");
+		balance += 100;
+	}
+	else if (playerHand > dealerHand && playerHand <= 21) {
+		console.log("You win!");
+		balance += 100;
+	}
+	else {
+		console.log("You lose.");
+	}
+	updateBalance();
+	closeBlackjack();
+}
+function dealCard() {
+	let card = Math.floor(Math.random() * 10) + 1;
+	console.log(card);
+	return card;
+}
+function blackjack() {
+	if(dealerHand <= 16) {
+		dealerHand += dealCard();
+	}
+	playerHand += dealCard();
+	
+	if (playerHand >= 21 || dealerHand >= 21) {
+		stay();
+	}
+	console.log(`Dealer Hand: ${dealerHand}`);
+	console.log(`Player Hand: ${playerHand}`);
+}
 
+//Slot Machine
 function updateBalance() {
   balanceText.textContent = `Balance: $${balance}`;
   localStorage.setItem('balance', balance);
@@ -42,32 +95,7 @@ function slotMachine() {
   updateBalance();
 }
 
-function blackjack() {
-	//Bet $10
-	//Double if win
-	//Hit or Stay
-	//Dealer and player
-	
-	balance -= 10;
-	updateBalance();
-	
-	let playerHand = 0;
-	let dealerHand = 0;
-	
-	let choice = true;
-	
-	while (choice || playerHand >= 21) {
-		dealerHand += dealCard();
-		playerHand += dealCard();
-	}
-	
-	
-}
-
-function dealCard() {
-	return Math.floor(Math.random() * 10) + 1;
-}
-
 spinBtn.addEventListener("click", slotMachine);
 blackjackOpenBtn.addEventListener("click", openBlackjack);
-blackjackCloseBtn.addEventListener("click", closeBlackjack);
+hitBtn.addEventListener("click", hit);
+stayBtn.addEventListener("click", stay);
